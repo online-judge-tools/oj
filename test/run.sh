24e4e0f46505105398c7023a42656b313f4c0495
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-01-08 15:08:34 +0900

    test

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e  # If not interactive, exit immediately if any untested command fails.
 cd $(dirname $0)
-for testcase in `find test -name test.sh` ; do
+for testcase in `find . -name test.sh` ; do
     echo $testcase
     bash $testcase
     echo PASSED
--- test/yukicoder/51/test.sh
+++ test/yukicoder/51/test.sh
@@ -2,12 +2,5 @@
 set -e  # If not interactive, exit immediately if any untested command fails.
 cd $(dirname $0)
 [ -e test ] && rm -r test
-oj download http://yukicoder.me/problems/100
-diff <(md5sum test/sample-*) - <<EOF
-c8a8eeb947c8a1d6700d6f7fd151cb00  test/sample-1.in
-3bb50ff8eeb7ad116724b56a820139fa  test/sample-1.out
-3b6feb4b7d767c8e7314f59a1749d544  test/sample-2.in
-9f4c0b1fca5cb6f886aa2e54442b1e1b  test/sample-2.out
-c12ec911666a5d65bf53e234291e402c  test/sample-3.in
-63a98316f78c5127e702db8fbea612a6  test/sample-3.out
-EOF
+oj download $(cat url)
+diff <(md5sum test/sample-*) md5.sum
