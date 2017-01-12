32f5841622a51849a5f86d73e4ef7cb3b356b3e0
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-01-12 16:20:04 +0900

    write messages for --help

--- onlinejudge/main.py
+++ onlinejudge/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import onlinejudge
 import onlinejudge.implementation.utils as utils
 import onlinejudge.implementation.logging as log
@@ -97,30 +98,76 @@ def submit(args):
         problem.submit(code, language=args.language, session=sess)
 
 def main(args=None):
+
     # argparse
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Tools for online judge services')
     parser.add_argument('-v', '--verbose', action='store_true')
-    parser.add_argument('-c', '--cookie', default=os.path.join(default_data_dir, 'cookie.jar'))
+    default_cookie_path = os.path.join(default_data_dir, 'cookie.jar')
+    parser.add_argument('-c', '--cookie', default=default_cookie_path,
+            help='default: {}'.format(default_cookie_path))
     parser.add_argument('-x', '--extra-option', action='append', default=[])
-    subparsers = parser.add_subparsers(dest='command')
+    subparsers = parser.add_subparsers(dest='command', help='for details, see "{} COMMAND --help"'.format(sys.argv[0]))
 
     # download
-    subparser = subparsers.add_parser('download')
+    subparser = subparsers.add_parser('download', help='download sample cases',
+            formatter_class=argparse.RawTextHelpFormatter,
+            epilog='''\
+supported services:
+  AnarchyGolf
+  AtCoder
+  Codeforces
+  HackerRank
+  Yukicoder
+
+format string for --format:
+  %i                    index: 1, 2, 3, ...
+  %e                    extension: "in" or "out"
+  %n                    name: e.g. "Sample Input 1", "system_test3.txt", ...
+  %b                    os.path.basename(name)
+  %d                    os.path.dirname(name)
+  %%                    '%' itself
+
+extra opitons via -x:
+  -x all                for yukicoder, use "テストケース一括ダウンロード"
+''')
     subparser.add_argument('url')
-    subparser.add_argument('-f', '--format')
+    subparser.add_argument('-f', '--format', help='a format string to specify paths of cases')
     subparser.add_argument('--overwrite', action='store_true')
+
     # login
-    subparser = subparsers.add_parser('login')
+    subparser = subparsers.add_parser('login', help='login to a service',
+            formatter_class=argparse.RawTextHelpFormatter,
+            epilog='''\
+supported services:
+  AtCoder
+  Codeforces
+  HackerRank
+  Yukicoder
+
+extra opitons via -x:
+  -x method=github      for yukicoder, login via github
+  -x method=twitter     for yukicoder, login via github (not implementated yet)
+''')
     subparser.add_argument('url')
     subparser.add_argument('-u', '--username')
     subparser.add_argument('-p', '--password')
+
     # submit
-    subparser = subparsers.add_parser('submit')
+    subparser = subparsers.add_parser('submit', help='submit your solution',
+            formatter_class=argparse.RawTextHelpFormatter,
+            epilog='''\
+supported services:
+  Yukicoder
+''')
     subparser.add_argument('url')
     subparser.add_argument('file')
     subparser.add_argument('-l', '--language')
+
     # test
-    subparser = subparsers.add_parser('test')
+    subparser = subparsers.add_parser('test', help='test your code (not implementated yet)',
+            formatter_class=argparse.RawTextHelpFormatter,
+            epilog='''\
+''')
 
     args = parser.parse_args(args=args)
 
