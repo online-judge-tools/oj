5be92d2cae2420140d678e76bf67f040623cff24
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-01-12 16:22:41 +0900

    sdist upload

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='online-judge-tools',
-    version='0.1.1',
+    version='0.1.2',
     description='Tools for online-judge services',
     install_requires=[
         'requests',
