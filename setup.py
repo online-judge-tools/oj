d97db4efe9726020d5c0b9393e648f80e8ff2c82
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-09-14 18:26:09 +0900

    version 0.1.17

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('readme.md') as fh:
 
 setup(
     name='online-judge-tools',
-    version='0.1.16',
+    version='0.1.17',
     description='Tools for online-judge services',
     install_requires=[
         'requests',
