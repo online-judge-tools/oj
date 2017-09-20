98172a18d4ca2b11a36f25f7f67d6fc7616e9d2c
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-09-21 00:25:33 +0900

    version 0.1.19

--- onlinejudge/implementation/version.py
+++ onlinejudge/implementation/version.py
@@ -3,5 +3,5 @@ __author__  = 'Kimiyuki Onaka'
 __email__ = 'kimiyuki95@gmail.com'
 __license__ = 'MIT License'
 __url__ = 'https://github.com/kmyk/online-judge-tools'
-__version_info__ = ( 0, 1, 18, 'final', 0 )
+__version_info__ = ( 0, 1, 19, 'final', 0 )
 __version__ = '.'.join(map(str, __version_info__[: 3]))
