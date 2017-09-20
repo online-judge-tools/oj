dd16fb19a3028c57a5855f5193ecb3b967f09a93
Kimiyuki Onaka <kimiyuki95@gmail.com>
2017-09-21 01:48:51 +0900

    複数の変数を上手くまとめるように

--- tests/generate_scanner.py
+++ tests/generate_scanner.py
@@ -21,7 +21,7 @@ class GenerateScannerTest(unittest.TestCase):
         log.addHandler(handler)
 
     def call_generate_scanner(self, url, expected, options=[]):
-        cmd = [ self.ojtools, 'generate-scanner', url ] + options
+        cmd = [ self.ojtools, '-v', 'generate-scanner', url ] + options
         output = subprocess.check_output(cmd, stderr=sys.stderr).decode()
         log.status('result:\n%s', output)
         if expected != output:
@@ -43,12 +43,48 @@ class GenerateScannerTest(unittest.TestCase):
     def test_call_generate_scanner_yukicoder_vector(self):
         self.call_generate_scanner(
             url='https://yukicoder.me/problems/no/4',
-            expected='int N; cin >> N;\nvector<int> W(N); for (int i = 0; i < N; ++ i) cin >> W[i];\n',
+            expected=''.join([
+                'int N; cin >> N;\n',
+                'vector<int> W(N); for (int i = 0; i < N; ++ i) cin >> W[i];\n',
+                ]),
             )
         self.call_generate_scanner(
             url='https://yukicoder.me/problems/no/4',
-            expected='int N; scanf("%d", &N);\nvector<int> W(N); REP (i, N) scanf("%d", &W[i]);\n',
+            expected=''.join([
+                'int N; scanf("%d", &N);\n',
+                'vector<int> W(N); REP (i, N) scanf("%d", &W[i]);\n',
+                ]),
             options=[ '--scanf', '--repeat-macro', 'REP' ]
+            )
+
+    def test_call_generate_scanner_yukicoder_vars(self):
+        self.call_generate_scanner(
+            url='https://yukicoder.me/problems/no/11',
+            expected=''.join([
+                'int W, H, N; cin >> W >> H >> N;\n',
+                'vector<int> S(N), K(N); for (int i = 0; i < N; ++ i) cin >> S[i] >> K[i];\n',
+                ]),
+            )
+
+    def test_call_generate_scanner_yukicoder_others(self):
+        self.call_generate_scanner(
+            url='https://yukicoder.me/problems/no/1',
+            expected=''.join([
+                'int N, C, V; cin >> N >> C >> V;\n',
+                'vector<int> S(V); REP (i, V) cin >> S[i];\n',
+                'vector<int> T(V); REP (i, V) cin >> T[i];\n',
+                'vector<int> Y(V); REP (i, V) cin >> Y[i];\n',
+                'vector<int> M(V); REP (i, V) cin >> M[i];\n',
+                ]),
+            options=[ '--repeat-macro', 'REP' ],
+            )
+        self.call_generate_scanner(
+            url='https://yukicoder.me/problems/no/12',
+            expected=''.join([
+                'int N; cin >> N;\n',
+                'vector<int> A(N); REP (i, N) cin >> A[i];\n',
+                ]),
+            options=[ '--repeat-macro', 'REP' ],
             )
 
 
