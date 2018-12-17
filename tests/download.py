38f538268c3d9b1866f2e4bb30c92a26051586b6
Kimiyuki Onaka <kimiyuki95@gmail.com>
2018-12-17 22:07:39 +0900

    #201: bug fix

--- tests/download.py
+++ tests/download.py
@@ -16,7 +16,7 @@ def get_files_from_json(samples):
                 name = sample['name'] + '.' + ext
             else:
                 name = 'sample-{}.{}'.format(i + 1, ext)
-            files[name] = sample[ext + 'put']
+            files[name] = hashlib.md5(sample[ext + 'put'].encode()).hexdigest()
     return files
 
 def snippet_call_download(self, url, files, is_system=False, type='files'):
