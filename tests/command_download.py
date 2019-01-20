debfe0d78d1f17d972130d4f7706ecb99e9ca89e
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-01-21 03:44:23 +0900

    #240: refactoring of tests/

--- tests/command_download.py
+++ tests/command_download.py
@@ -1,12 +1,11 @@
 import unittest
 
+import tests.utils
+
 import hashlib
 import os
-import os.path
-import shutil
 import subprocess
 import sys
-import tempfile
 
 def get_files_from_json(samples):
     files = {}
@@ -24,13 +23,8 @@ def snippet_call_download(self, url, files, is_system=False, type='files'):
     if type == 'json':
         files = get_files_from_json(files)
 
-    cwd = os.getcwd()
-    ojtools = os.path.join( cwd, 'oj' )
-    try:
-        tempdir = tempfile.mkdtemp()
-        os.chdir(tempdir)
-        if os.path.exists('test'):
-            shutil.rmtree('test')
+    ojtools = os.path.abspath('oj')
+    with tests.utils.sandbox([]):
         cmd = [ ojtools, 'download', url ]
         if is_system:
             cmd += [ '--system' ]
@@ -41,6 +35,3 @@ def snippet_call_download(self, url, files, is_system=False, type='files'):
                 with open(os.path.join('test', name)) as fh:
                     result[name] = hashlib.md5(fh.buffer.read()).hexdigest()
         self.assertEqual(files, result)
-    finally:
-        os.chdir(cwd)
-        shutil.rmtree(tempdir)
---
+++ tests/utils.py
@@ -0,0 +1,41 @@
+import contextlib
+import os
+import pathlib
+import subprocess
+import sys
+import tempfile
+
+
+@contextlib.contextmanager
+def chdir(path):
+    cwd = os.getcwd()
+    try:
+        os.chdir(path)
+        yield
+    finally:
+        os.chdir(cwd)
+
+def prepare_files(files):
+    for f in files:
+        path = pathlib.Path(f['path'])
+        path.parent.mkdir(parents=True, exist_ok=True)
+        with open(str(path), 'w') as fh:
+            fh.write(f['data'])
+        if f.get('executable', False):
+            path.chmod(0o755)
+
+@contextlib.contextmanager
+def sandbox(files):
+    with tempfile.TemporaryDirectory() as tempdir:
+        with chdir(tempdir):
+            prepare_files(files)
+            yield tempdir
+
+def run_in_sandbox(args, files):
+    ojtools = os.path.abspath('oj')
+    with sandbox(files) as tempdir:
+        proc = subprocess.run([ ojtools ] + args, stdout=subprocess.PIPE, stderr=sys.stderr)
+        return {
+            'proc': proc,
+            'tempdir': tempdir,
+        }
