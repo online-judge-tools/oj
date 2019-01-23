2448f1d6835129bc08855a9ecc59fea347a14243
Pachicobue <tigerssho@gmail.com>
2019-01-24 00:18:17 +0900

    add re.escape for match_with_format

--- onlinejudge/implementation/format_utils.py
+++ onlinejudge/implementation/format_utils.py
@@ -9,6 +9,7 @@ import re
 import sys
 from typing import Dict, List, Match, Optional
 
+
 def glob_with_format(directory: pathlib.Path, format: str) -> List[pathlib.Path]:
     table = {}
     table['s'] = '*'
@@ -19,12 +20,15 @@ def glob_with_format(directory: pathlib.Path, format: str) -> List[pathlib.Path]
         log.debug('testcase globbed: %s', path)
     return paths
 
+
 def match_with_format(directory: pathlib.Path, format: str, path: pathlib.Path) -> Optional[Match[str]]:
     table = {}
     table['s'] = '(?P<name>.+)'
     table['e'] = '(?P<ext>in|out)'
-    pattern = re.compile('^' + str(directory.resolve()) + '/' + utils.percentformat(format, table) + '$')
+    pattern = re.compile('^' + re.escape(str(directory.resolve())) +
+                         '/' + utils.percentformat(re.escape(format), table) + '$')
     return pattern.match(str(path.resolve()))
+
 
 def path_from_format(directory: pathlib.Path, format: str, name: str, ext: str) -> pathlib.Path:
     table = {}
@@ -32,9 +36,11 @@ def path_from_format(directory: pathlib.Path, format: str, name: str, ext: str) 
     table['e'] = ext
     return directory / utils.percentformat(format, table)
 
+
 def is_backup_or_hidden_file(path: pathlib.Path) -> bool:
     basename = path.stem
     return basename.endswith('~') or (basename.startswith('#') and basename.endswith('#')) or basename.startswith('.')
+
 
 def drop_backup_or_hidden_files(paths: List[pathlib.Path]) -> List[pathlib.Path]:
     result = []  # type: List[pathlib.Path]
@@ -42,18 +48,20 @@ def drop_backup_or_hidden_files(paths: List[pathlib.Path]) -> List[pathlib.Path]
         if is_backup_or_hidden_file(path):
             log.warning('ignore a backup file: %s', path)
         else:
-            result += [ path ]
+            result += [path]
     return result
 
+
 def construct_relationship_of_files(paths: List[pathlib.Path], directory: pathlib.Path, format: str) -> Dict[str, Dict[str, pathlib.Path]]:
-    tests = collections.defaultdict(dict)  # type: Dict[str, Dict[str, pathlib.Path]]
+    # type: Dict[str, Dict[str, pathlib.Path]]
+    tests = collections.defaultdict(dict)
     for path in paths:
         m = match_with_format(directory, format, path.resolve())
         if not m:
             log.error('unrecognizable file found: %s', path)
             sys.exit(1)
         name = m.groupdict()['name']
-        ext  = m.groupdict()['ext']
+        ext = m.groupdict()['ext']
         assert ext not in tests[name]
         tests[name][ext] = path
     for name in tests:
