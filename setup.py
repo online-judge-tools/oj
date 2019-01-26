ab9b703ff03bf3429de0f82a8d7390e5f4b60b33
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-01-26 17:05:47 +0900

    #243: fix the problem of onlinejudge/__about__.py and onlinejudge/__init__.py

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python3
+import imp
+import os
+
 from setuptools import setup
-setup()
+
+
+def load_module(module_path):
+    path = None
+    for name in module_path.split('.'):
+        file, path, description = imp.find_module(name, path)
+        path = [path]
+    return imp.load_module(name, file, path[0], description)
+
+
+version = load_module('onlinejudge.__about__')
+
+setup(
+    name=version.__package_name__,
+    version=version.__version__,
+    author=version.__author__,
+    author_email=version.__email__,
+    url=version.__url__,
+    license=version.__license__,
+)
