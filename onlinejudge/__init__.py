aa858bcac4287e75c38f6b10a11eaf9ed278714d
Unknown <nannyakannya@gmail.com>
2019-01-28 08:41:56 +0900

    clean up __init__

--- onlinejudge/__init__.py
+++ onlinejudge/__init__.py
@@ -1,7 +1,4 @@
 # Python Version: 3.x
-from onlinejudge.__about__ import *
 import onlinejudge.dispatch
 import onlinejudge.service
-import onlinejudge.__about__
-
-__version__ = onlinejudge.__about__.__version__
+from onlinejudge.__about__ import __version__
