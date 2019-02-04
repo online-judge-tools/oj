b91cfc3adfd583f1c27d24880bb345bd6650ed9b
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-02-04 23:10:31 +0900

    #208: use @unittest.skip with the reason instead of just commenting out

--- tests/command_download_hackerrank.py
+++ tests/command_download_hackerrank.py
@@ -7,13 +7,15 @@ class DownloadHackerRankTest(unittest.TestCase):
     def snippet_call_download(self, *args, **kwargs):
         tests.command_download.snippet_call_download(self, *args, **kwargs)
 
-    # def test_call_download_hackerrank_beautiful_array(self):
-    #     self.snippet_call_download('https://www.hackerrank.com/contests/hourrank-1/challenges/beautiful-array', {
-    #         'sample-1.in': 'fb3f7e56dac548ce73f9d8e485e5336b',
-    #         'sample-2.out': '897316929176464ebc9ad085f31e7284',
-    #         'sample-2.in': '6047a07c8defde4d696513d26e871b20',
-    #         'sample-1.out': '6d7fce9fee471194aa8b5b6e47267f03',
-    #     })
+    # TODO: support parsing HTML or retrieving from "Run Code" feature
+    @unittest.skip('the "Download all test cases" feature is not supported for this problem')
+    def test_call_download_hackerrank_beautiful_array(self):
+        self.snippet_call_download('https://www.hackerrank.com/contests/hourrank-1/challenges/beautiful-array', {
+            'sample-1.in': 'fb3f7e56dac548ce73f9d8e485e5336b',
+            'sample-2.out': '897316929176464ebc9ad085f31e7284',
+            'sample-2.in': '6047a07c8defde4d696513d26e871b20',
+            'sample-1.out': '6d7fce9fee471194aa8b5b6e47267f03',
+        })
 
     def test_call_download_hackerrank_hourrank_30_a_system(self):
         # TODO: these file names should "00.in", "00.out", ..., "10.out"
