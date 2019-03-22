d0fb6e967e5690901b49e8d2299bf807682967e9
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-03-22 16:34:02 +0900

    NONE: move some tests from tests/command_download_aoj.py to tests/service_aoj.py

--- tests/command_download_aoj.py
+++ tests/command_download_aoj.py
@@ -8,18 +8,6 @@ class DownloadAOJTest(unittest.TestCase):
     def snippet_call_download(self, *args, **kwargs):
         tests.command_download.snippet_call_download(self, *args, **kwargs)
 
-    def test_call_download_aoj_DSL_1_A(self):
-        self.snippet_call_download('http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_A', {
-            'sample-1.in': 'cb3a243a13637cddedf245cd0f6eab86',
-            'sample-1.out': '29cc7a34bb5a15da3d14ef4a82a4c530',
-        })
-
-    def test_call_download_aoj_0100(self):
-        self.snippet_call_download('http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0100', {
-            'sample-1.in': '4f0f7b3b0b73c97c5283395edde3dbe8',
-            'sample-1.out': '26d3b085a160c028485f3865d07b9192',
-        })
-
     def test_call_download_aoj_1371(self):
         self.snippet_call_download('http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1371', {
             'sample-6.in': '3521658c02c291ad5a4e5cbaa3cb0260',
@@ -36,12 +24,6 @@ class DownloadAOJTest(unittest.TestCase):
             'sample-1.in': '56092c4794d713f93d2bb70a66aa6ca1',
             'sample-4.in': '318d4b3abfa30cc8fad4b1d34430aea3',
             'sample-7.in': 'dcac31a5a6542979ce45064ab0bfa83d',
-        })
-
-    def test_call_download_aoj_2256(self):
-        self.snippet_call_download('http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2256&lang=jp', {
-            'sample-1.in': 'c89817f1ee0b53209d66abc94e457f7f',
-            'sample-1.out': 'b9c2c5761360aad068453f4e64dd5a4e',
         })
 
     def test_call_download_aoj_2310(self):
@@ -62,22 +44,6 @@ class DownloadAOJTest(unittest.TestCase):
         self.snippet_call_download('http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2511', {
             'sample-1.in': '0483a0080de977d5e1db1ab87eae3fa9',
             'sample-1.out': '346ce6367eff6bb3c9915601f2ae1e75',
-        })
-
-    def test_call_download_aoj_DSL_3_B(self):
-        self.snippet_call_download('https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/3/DSL_3_B', {
-            'sample-1.in': '36adbcbb268e04ef7667fb2d965eed2c',
-            'sample-1.out': '26ab0db90d72e28ad0ba1e22ee510510',
-            'sample-2.in': '89a4280a03ec0001ec91f3fedbafadc1',
-            'sample-2.out': '6d7fce9fee471194aa8b5b6e47267f03',
-            'sample-3.in': '22d823cf994ebee157a0cdc8219a600d',
-            'sample-3.out': '897316929176464ebc9ad085f31e7284',
-        })
-
-    def test_call_download_aoj_2394(self):
-        self.snippet_call_download('https://onlinejudge.u-aizu.ac.jp/challenges/sources/JAG/Spring/2394?year=2011', {
-            'sample-1.in': '05dfaf25ae93e601a10cfb278db7679c',
-            'sample-1.out': '80982df7f6dac58f828e2e8b12e87a0a',
         })
 
     def test_call_download_aoj_system_ITP1_1_B(self):
