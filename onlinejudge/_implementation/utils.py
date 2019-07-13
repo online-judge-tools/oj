f3502cba6224d6f3890da897748880c613cf2089
Unknown <nannyakannya@gmail.com>
2019-07-13 12:06:35 +0900

    a

--- onlinejudge/_implementation/utils.py
+++ onlinejudge/_implementation/utils.py
@@ -151,8 +151,8 @@ def exec_command(command_str: str, *, stdin: IO[Any], timeout: Optional[float] =
         print(command.encode())
         try:
             import locale
-            proc = subprocess.Popen(command_str, stdin=stdin, stdout=subprocess.PIPE, stderr=sys.stderr,
-                                    #encoding=locale.getdefaultlocale()[1]
+            proc = subprocess.Popen(command.encode(), stdin=stdin, stdout=subprocess.PIPE, stderr=sys.stderr,
+                                    encoding=locale.getdefaultlocale()[1]
                                     )
         except FileNotFoundError:
             log.error('No such file or directory: %s', command)
