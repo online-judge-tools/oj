0307eeb04c9ca5cdb09667ab800ef952fb7f046d
Unknown <nannyakannya@gmail.com>
2019-07-13 12:27:54 +0900

    #468: for mypy

--- onlinejudge/_implementation/utils.py
+++ onlinejudge/_implementation/utils.py
@@ -144,8 +144,8 @@ def exec_command(command_str: str, *, stdin: IO[Any], timeout: Optional[float] =
         if gnu_time is not None:
             command = [gnu_time, '-f', '%M', '-o', fh.name, '--'] + command
         if os.name == 'nt':
-            command = command_str  # type: ignore
-            command = command.encode().decode()  # HACK: for multi thread unstable
+            # HACK: for multi thread unstable
+            command = command_str.encode().decode()  # type: ignore
         begin = time.perf_counter()
 
         try:
