bb0d383c500258c1589e735fb138c7ac1b45b9d5
fukatani <nannyakannya@gmail.com>
2019-10-14 22:52:40 +0900

    #438: Add get_latest_version_test

--- tests/command_version.py
+++ tests/command_version.py
@@ -1,8 +1,10 @@
+import os
 import unittest
 
 import tests.utils
 
 import onlinejudge
+from onlinejudge._implementation import utils
 
 
 class PrintVersionTest(unittest.TestCase):
@@ -12,3 +14,13 @@ class PrintVersionTest(unittest.TestCase):
 
         result = tests.utils.run_in_sandbox(args=['-version, test'])
         self.assertTrue(result['proc'].stdout == 'online-judge-tools {}'.format(onlinejudge.__version__))
+
+    def get_latest_version_test(self):
+        version_cache_path = utils.cache_dir / "pypi.json"
+
+        # without cache
+        os.remove(version_cache_path)
+        self.assertEqual(utils.get_latest_version_from_pypi(), onlinejudge.__version__)
+
+        # from cache
+        self.assertEqual(utils.get_latest_version_from_pypi(), onlinejudge.__version__)
