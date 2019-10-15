3f2d39dbcad8829d9437eb38d7e712c819e8c2d9
fukatani <nannyakannya@gmail.com>
2019-10-16 00:03:31 +0900

    #438: Add dispatch test for codeforces.

--- tests/dispatch.py
+++ tests/dispatch.py
@@ -3,7 +3,7 @@ import unittest
 from onlinejudge import dispatch, service
 
 
-class DispatchTest(unittest.TestCase):
+class DispatchAtCoderTest(unittest.TestCase):
     def test_problem_from_url(self):
         problem = dispatch.problem_from_url('https://atcoder.jp/contests/arc001/tasks/arc001_1')
         self.assertTrue(isinstance(problem, service.atcoder.AtCoderProblem))
@@ -25,7 +25,31 @@ class DispatchTest(unittest.TestCase):
 
     def test_contest_from_url(self):
         contest = dispatch.contest_from_url('https://atcoder.jp/contests/agc030')
+        self.assertTrue(isinstance(contest, service.atcoder.AtCoderContest))
         self.assertTrue(isinstance(contest.get_service(), service.atcoder.AtCoderService))
+
+    def test_service_from_url(self):
+        service1 = dispatch.service_from_url('https://atcoder.jp/')
+        self.assertTrue(isinstance(service1, service.atcoder.AtCoderService))
+        self.assertEqual(service1.get_url_of_login_page(), 'https://atcoder.jp/login')
+        self.assertEqual(service1.get_user_history_url(22), 'https://atcoder.jp/users/22/history/json')
+
+
+class DispatchCodeForcesTest(unittest.TestCase):
+    def test_problem_from_url(self):
+        problem = dispatch.problem_from_url('https://codeforces.com/contest/1244/problem/C')
+        self.assertTrue(isinstance(problem, service.codeforces.CodeforcesProblem))
+        self.assertTrue(isinstance(problem.get_service(), service.codeforces.CodeforcesService))
+
+    def test_contest_from_url(self):
+        contest = dispatch.contest_from_url('https://codeforces.com/contest/1244')
+        self.assertTrue(isinstance(contest, service.codeforces.CodeforcesContest))
+        self.assertTrue(isinstance(contest.get_service(), service.codeforces.CodeforcesService))
+
+    def test_service_from_url(self):
+        service1 = dispatch.service_from_url('https://codeforces.com/')
+        self.assertTrue(isinstance(service1, service.codeforces.CodeforcesService))
+        self.assertEqual(service1.get_url_of_login_page(), 'https://codeforces.com/enter')
 
 
 class InvalidDispatchTest(unittest.TestCase):
@@ -36,4 +60,7 @@ class InvalidDispatchTest(unittest.TestCase):
         self.assertIsNone(dispatch.submission_from_url('https://atcoder.jp/contests/agc039'))
 
     def test_contest_from_url(self):
-        self.assertIsNone(dispatch.contest_from_url('https://www.yahoo.co.jp/'))
+        self.assertIsNone(dispatch.contest_from_url('https://atcoder.jp/'))
+
+    def test_service_from_url(self):
+        self.assertIsNone(dispatch.service_from_url('https://www.yahoo.co.jp/'))
