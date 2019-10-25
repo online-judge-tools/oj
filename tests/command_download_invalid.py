2c2b2af6140ec8690af5b6d1597ec34494c03d12
Unknown <nannyakannya@gmail.com>
2019-10-25 16:31:03 +0900

    Add download twice test.

--- tests/command_download_invalid.py
+++ tests/command_download_invalid.py
@@ -8,5 +8,24 @@ class DownloadInvalid(unittest.TestCase):
     def snippet_call_download_raises(self, *args, **kwargs):
         tests.command_download.snippet_call_download_raises(self, *args, **kwargs)
 
+    def snippet_call_download_twice(self, *args, **kwargs):
+        tests.command_download.snippet_call_download_twice(self, *args, **kwargs)
+
     def test_call_download_invalid(self):
         self.snippet_call_download_raises(requests.exceptions.InvalidURL, 'https://not_exist_contest.jp/tasks/001_a')
+
+    def test_call_download_twice(self):
+        self.snippet_call_download_twice('https://atcoder.jp/contests/abc114/tasks/abc114_c', 'https://atcoder.jp/contests/abc003/tasks/abc003_4', [
+            {
+                "input": "575\n",
+                "output": "4\n"
+            },
+            {
+                "input": "3600\n",
+                "output": "13\n"
+            },
+            {
+                "input": "999999999\n",
+                "output": "26484\n"
+            },
+        ], type='json')
