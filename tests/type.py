b01d0b58afad9de087925b01e231d29448c5451f
fukatani <nannyakannya@gmail.com>
2019-11-03 11:50:02 +0900

    Add unittests for __eq__

--- tests/type.py
+++ tests/type.py
@@ -1,5 +1,6 @@
 import unittest
 
+from onlinejudge.dispatch import problem_from_url, service_from_url, submission_from_url
 from onlinejudge.type import Problem, Service, Submission
 
 
@@ -8,3 +9,16 @@ class TypeTest(unittest.TestCase):
         self.assertRaises(TypeError, Service)
         self.assertRaises(TypeError, Problem)
         self.assertRaises(TypeError, Submission)
+
+    def test_service_eq(self):
+        self.assertEqual(service_from_url('https://atcoder.jp/'), service_from_url('https://atcoder.jp/contests/agc030'))
+        self.assertNotEqual(service_from_url('https://atcoder.jp/'), service_from_url('https://codeforces.com/'))
+
+    def test_problem_eq(self):
+        self.assertEqual(problem_from_url('https://codeforces.com/contest/1244/problem/C'), problem_from_url('https://codeforces.com/contest/1244/problem/C'))
+        self.assertNotEqual(problem_from_url('https://codeforces.com/contest/1244/problem/C'), problem_from_url('https://codeforces.com/contest/1244/problem/B'))
+        self.assertNotEqual(problem_from_url('https://codeforces.com/contest/1244/problem/C'), problem_from_url('https://atcoder.jp/contests/abc143/tasks/abc143_c'))
+
+    def test_submission_eq(self):
+        self.assertEqual(submission_from_url('https://atcoder.jp/contests/abc143/submissions/8264863'), submission_from_url('https://atcoder.jp/contests/abc143/submissions/8264863'))
+        self.assertNotEqual(submission_from_url('https://atcoder.jp/contests/abc143/submissions/8264863'), submission_from_url('https://atcoder.jp/contests/abc143/submissions/8264897'))
