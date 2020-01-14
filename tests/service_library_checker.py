28d03bd92992000e83397d33c03b8434209f8b50
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-01-14 18:20:36 +0900

    Disable tests for Library Checker on Windows

--- tests/service_library_checker.py
+++ tests/service_library_checker.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import unittest
 
@@ -19,11 +20,13 @@ class LibraryCheckerProblemTest(unittest.TestCase):
     def test_from_url(self):
         self.assertEqual(LibraryCheckerProblem.from_url('https://judge.yosupo.jp/problem/point_add_range_sum').problem_id, 'point_add_range_sum')
 
+    @unittest.skipIf(os.name == 'nt', "Library Checker is not supported on Windows")
     def test_download_samples(self):
         self.assertEqual(LibraryCheckerProblem.from_url('https://judge.yosupo.jp/problem/unionfind').download_sample_cases(), [
             TestCase(name='example_00', input_name='example_00.in', input_data=b'4 7\n1 0 1\n0 0 1\n0 2 3\n1 0 1\n1 1 2\n0 0 2\n1 1 3\n', output_name='example_00.out', output_data=b'0\n1\n0\n1\n'),
         ])
 
+    @unittest.skipIf(os.name == 'nt', "Library Checker is not supported on Windows")
     def test_pull_repository(self):
         # reset
         LibraryCheckerService.is_repository_updated = False
